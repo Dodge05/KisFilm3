9660b8fb1f0f374c749d72574ad0e2be701a393e
Dodge05 <85215704+Dodge05@users.noreply.github.com>
2024-03-05 18:43:34 +0100

    First

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+    open.style.display = 'none'
+
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
